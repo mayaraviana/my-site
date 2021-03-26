17ad3febc75a833b68b2cb4a893298a14ffb4c9e
Mayara Viana <mayaraviana60@hotmail.com>
2021-03-26 00:46:11 -0300

    bootstrap

---
+++ src/components/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hero">
+    <h1>
+      &lt;Hi, you found me! I am a front-end developer from Brazil. Currently
+      working to build awesome things. / &gt;
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style lang="scss">
+</style>
